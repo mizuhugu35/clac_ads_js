4d6839cd1a6180b335e78f089b697f0c2d2e297b
KikuchiTomo <tomo_kikuchi@icloud.com>
2020-09-04 20:14:34 +0900

    add code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # clac_ads_js
+## Usage 
+    If you want to use this tool , click <a href="./">this URL</a>.    
+    
+## Notes
+    This JavaScript code does not handle any error handling, so you need to enter all the parameters.  
+    So, i cannot be held responsible for any trouble by the results of this code.
---
+++ calc.js
@@ -0,0 +1,71 @@
+multi_table_ =
+      [[1.0, 0.9, 0.6],
+       [1.5, 0.59, 0.59],
+       [2.0, 0.49, 0.49],
+       [2.5, 0.42, 0.42],
+       [3.0, 0.35, 0.35],
+       [4.0, 0.3,  0.3],
+       [5.0, 0.22, 0.22],
+       [12.0, 0.092, 0.14]];
+
+PI_ = 3.14159265358;
+
+function DecToRad(deg){
+    return deg*(PI_/180.0);
+}
+
+function RadToDec(rad){
+    return rad*(180.0/PI_);
+}
+
+function HorizontalFov(vertical_fov, aspect_ratio_x, aspect_ratio_y){
+    let AspectRatio = (aspect_ratio_x / aspect_ratio_y);
+    let VerticalFovHalf_rad = DecToRad(vertical_fov/2.0);
+    let Arctan_arg = (Math.tan(VerticalFovHalf_rad) * AspectRatio);
+    return 2 * RadToDec(Math.atan(Arctan_arg));
+}
+
+function VerticalFov(aspect_ratio_x, aspect_ratio_y){
+    let AspectRatio = (aspect_ratio_x / aspect_ratio_y);
+    let Arctan_arg = Math.tan(DecToRad(75)/AspectRatio);
+    return 2 * Math.atan(Arctan_arg);
+}
+
+function FovAdjustment(fov_multi, vertical_fov){
+    let Tan_arg0 = DecToRad((fov_multi * vertical_fov)/ 2.0);
+    let Tan_arg1 = DecToRad(vertical_fov/2.0);
+    return Math.tan(Tan_arg0)/Math.tan(Tan_arg1);
+}
+
+function CalcNewADS(ads_multi, fov_adj, old_ads){
+    return (ads_multi/fov_adj)*old_ads;
+}
+
+function GenMultiTable(id){
+    var select_elem = document.getElementById(id);
+    for(let i=0; i<8; i++){
+        let elem = document.createElement('option');
+        elem.value = i;
+        elem.innerHTML = multi_table_[i][0];
+        select_elem.appendChild(elem);
+    }
+}
+
+function calc(h_fov, v_fov, fov_a, ap_x, ap_y, old_ads, new_ads){
+    var h_fov_e = document.getElementById(h_fov);
+    var v_fov_e = document.getElementById(v_fov);
+    var fov_a_e = document.getElementById(fov_a);
+    var ap_x_e  = document.getElementById(ap_x);
+    var ap_y_e  = document.getElementById(ap_y);
+    var old_ads_e = document.getElementById(old_ads);
+    var new_ads_e = document.getElementById(new_ads);
+    var table_idx = document.getElementById('multi-table').value;
+
+    var H_fov = HorizontalFov(v_fov_e.value, ap_x_e.value, ap_y_e.value);
+    var F_adj = FovAdjustment(multi_table_[table_idx][1], v_fov_e.value);
+    var N_ads = CalcNewADS(multi_table_[table_idx][2], F_adj, old_ads_e.value);
+
+    h_fov_e.value = H_fov;
+    fov_a_e.value = F_adj;
+    new_ads_e.value = N_ads;    
+}
