5d1c8d37d765a9aff92813ac720c0fdcb6dd8037
mizuhugu35 <mizuhugu35@gmail.com>
2020-09-07 22:41:44 +0900

    fixed

--- calc.js
+++ calc.js
@@ -11,14 +11,17 @@ multi_table_ =
 PI_ = 3.14159265358;
 
 function DecToRad(deg){
+    console.log("DEBUG: DegToRad called.");
     return deg*(PI_/180.0);
 }
 
 function RadToDec(rad){
+    console.log("DEBUG: RadToDeg called.");
     return rad*(180.0/PI_);
 }
 
 function HorizontalFov(vertical_fov, aspect_ratio_x, aspect_ratio_y){
+    console.log("DEBUG: calc horizontal fov.");
     let AspectRatio = (aspect_ratio_x / aspect_ratio_y);
     let VerticalFovHalf_rad = DecToRad(vertical_fov/2.0);
     let Arctan_arg = (Math.tan(VerticalFovHalf_rad) * AspectRatio);
@@ -26,6 +29,7 @@ function HorizontalFov(vertical_fov, aspect_ratio_x, aspect_ratio_y){
 }
 
 function VerticalFov(aspect_ratio_x, aspect_ratio_y){
+    console.log("DEBUG: calc vertical fov.");
     let AspectRatio = (aspect_ratio_x / aspect_ratio_y);
     let Arctan_arg = Math.tan(DecToRad(75)/AspectRatio);
     return 2 * Math.atan(Arctan_arg);
@@ -42,12 +46,16 @@ function CalcNewADS(ads_multi, fov_adj, old_ads){
 }
 
 function GenMultiTable(id){
+    console.log("DEBUG: [INFO] Genmultitable called.");
     var select_elem = document.getElementById(id);
+    if(select_elem==undefined) console.log("DEBUG: [ERROR] Multi table is undefined.");
+    
     for(let i=0; i<8; i++){
         let elem = document.createElement('option');
         elem.value = i;
-        elem.innerHTML = multi_table_[i][0];
+        elem.innerHTML = multi_table_[i][0];        
         select_elem.appendChild(elem);
+        console.log("DEBUG: [INFO] append child element to ID["+id+"]. child info->", elem);        
     }
 }
 
@@ -56,6 +64,8 @@ function isString(obj) {
 };
 
 function calc(h_fov, v_fov, fov_a, ap_x, ap_y, old_ads, new_ads){
+    console.log("DEBUG: starting calculation...");
+    
     var h_fov_e = document.getElementById(h_fov);
     var v_fov_e = document.getElementById(v_fov);
     var fov_a_e = document.getElementById(fov_a);
@@ -76,5 +86,7 @@ function calc(h_fov, v_fov, fov_a, ap_x, ap_y, old_ads, new_ads){
 
     h_fov_e.value = H_fov;
     fov_a_e.value = F_adj;
-    new_ads_e.value = N_ads;    
+    new_ads_e.value = N_ads;
+
+    console.log("DEBUG: finished calculation.");
 }
